001cb951a2dbfe2d68478faa2ca824ec5b527a41
LeandroRoberto <leandrorobertotec@uniplaclages.edu.br>
2023-05-14 11:06:28 -0300

    Criação dos migrations das tabelas do controle de Usuario.

---
+++ Financeiro.Solution.Infra.Data.Migrations/Migrations/InitialSeed_2023051402_AspNetUserClaims_Table.cs
@@ -0,0 +1,36 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financeiro.Solution.Infra.Data.Migrations.Migrations
+{
+    [Migration(20230514)]
+    public class InitialSeed_20230514_AspNetUserClaims_Table : Migration
+    {
+        public override void Down()
+        {
+
+            Delete.Table("AspNetUserClaims");
+
+        }
+
+        public override void Up()
+        {
+            Create.Table("AspNetUserClaims")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("RoleId").AsString(450)
+                .WithColumn("ClaimType").AsString()
+                .WithColumn("ClaimValue").AsString();
+
+            Create.ForeignKey("FK_AspNetUserClaims_AspNetUsers_UserId")
+           .FromTable("AspNetUsers").ForeignColumn("Id")
+           .ToTable("AspNetRoles").PrimaryColumn("Id")
+           .OnDelete(Rule.Cascade);
+        }
+    }
+}
+
