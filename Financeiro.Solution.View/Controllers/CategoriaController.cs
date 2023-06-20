a48ebf3c03ee18606381a3f5f45bd4784d3a4b40
LeandroRoberto <leandrorobertotec@uniplaclages.edu.br>
2023-06-19 21:02:09 -0300

    Criação prototipo de logs

--- Financeiro.Solution.View/Controllers/CategoriaController.cs
+++ Financeiro.Solution.View/Controllers/CategoriaController.cs
@@ -5,6 +5,8 @@ using FinanceiroSolution.Domain.Servicos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Serilog.Core;
 
 namespace Financeiro.Solution.View.Controllers
 {
@@ -15,27 +17,51 @@ namespace Financeiro.Solution.View.Controllers
     {
         private readonly InterfaceCategoria _InterfaceCategoria;
         private readonly ICategoriaServico _ICategoriaServico;
+        private readonly ILogger<CategoriaController> _logger;
 
-        public CategoriaController(InterfaceCategoria InterfaceCategoria, ICategoriaServico ICategoriaServico)
+        public CategoriaController(InterfaceCategoria InterfaceCategoria, ICategoriaServico ICategoriaServico, ILogger<CategoriaController> logger)
         {
             _InterfaceCategoria = InterfaceCategoria;
             _ICategoriaServico = ICategoriaServico;
+            _logger = logger;
         }
 
         [HttpGet("/api/ListarCategoriasUsuario")]
         [Produces("application/json")]
         public async Task<object> ListarCategoriasUsuario(string emailUsuario)
         {
-            return _InterfaceCategoria.ListarCategoriasUsuario(emailUsuario);
+            try
+            {
+                _logger.LogInformation("Executando o método ListarCategoriasUsuario");
+
+                var result = await _InterfaceCategoria.ListarCategoriasUsuario(emailUsuario);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ocorreu um erro ao executar o método ListarCategoriasUsuario");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro interno no servidor");
+            }
         }
 
         [HttpPost("/api/AdicionarCategoria")]
         [Produces("application/json")]
         public async Task<object> AdicionarCategoria(Categoria categoria)
         {
-            await _ICategoriaServico.AdicionarCategoria(categoria);
+            try
+            {
+                _logger.LogInformation("Executando o método AdicionarCategoria");
 
-            return categoria;
+                await _ICategoriaServico.AdicionarCategoria(categoria);
+
+                return categoria;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ocorreu um erro ao executar o método AdicionarCategoria");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro interno no servidor");
+            }
         }
     }
 }
