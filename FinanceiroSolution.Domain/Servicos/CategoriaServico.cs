af324fab3fe7b5767366f49ddcab77809b82de81
LeandroRoberto <leandrorobertotec@uniplaclages.edu.br>
2023-06-29 07:38:19 -0300

    Alterado o respositorio Categoria e melhorado as repostas para o controller categoria
    tambem estou tentando corrigir

--- FinanceiroSolution.Domain/Servicos/CategoriaServico.cs
+++ FinanceiroSolution.Domain/Servicos/CategoriaServico.cs
@@ -20,8 +20,16 @@ namespace FinanceiroSolution.Domain.Servicos
 
         public async Task AdicionarCategoria(Categoria catagoria)
         {
-            
+            try
+            {
                 await _interfaceCategoria.Adicionar(catagoria);
+            }
+            catch (Exception ex)
+            {
+                // Trate o erro aqui (por exemplo, registre o erro, envie uma resposta de erro, etc.)
+                Console.WriteLine("Ocorreu um erro ao adicionar a categoria: " + ex.Message);
+                throw; // Re-lança a exceção para que ela possa ser capturada no controller
+            }
         }
 
         public async Task AtualizarCategoria(Categoria catagoria)
